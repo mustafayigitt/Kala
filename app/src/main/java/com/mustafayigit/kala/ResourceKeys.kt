baa406b752ce8f99969e5ad0c7e4916b63111a0b
Mustafa Yiğit <mustafa.yigit@apsiyon.com>
2023-02-18 02:39:20 +0300

    create example resource keys

---
+++ app/src/main/java/com/mustafayigit/kala/ResourceKeys.kt
@@ -0,0 +1,23 @@
+package com.mustafayigit.kala
+
+import android.graphics.drawable.Drawable
+
+
+/**
+ * Created by Mustafa Yiğit on 17/02/2023
+ * mustafa.yt65@gmail.com
+ */
+object ResourceKeys {
+
+    // STRING KEYS
+    val HELLO = ResourceKey<String>("app.strings.hello")
+    val CHANGE_LANGUAGE = ResourceKey<String>("app.strings.change_language")
+    val SUCCESS = ResourceKey<String>("app.strings.success")
+
+    // OTHER KEYS
+    val TEXT_FROM_OUTSOURCE = ResourceKey<String>("app.strings.text_from_outsource")
+
+    // DRAWABLE KEYS
+    val COUNTRY_FLAG = ResourceKey<Drawable>("flag")
+
+}
